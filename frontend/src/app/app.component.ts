75d41f88c28a08d7b99598f29d6f8607591a9504
ianraphael <labicaniian@gmail.com>
2023-02-09 20:24:25 +0800

    Add ModalService to observe the modal properties

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ModalService } from './services/ui/modal.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'frontend';
+  showModal: boolean = false;
+  modalContent: string = '';
+  showModalSubscription = new Subscription();
+  modalContentSubscription = new Subscription();
+
+  constructor(private modalService: ModalService) {}
+
+  ngOnInit(): void {
+    this.showModalSubscription = this.modalService
+      .modalToggler()
+      .subscribe((val) => (this.showModal = val));
+
+    this.modalContentSubscription = this.modalService
+      .modalContentToggler()
+      .subscribe((val) => (this.modalContent = val));
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NotesComponent } from './components/notes/notes.component';
+import { NoteComponent } from './components/notes/note/note.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { ModalTogglerComponent } from './components/modal/modal-toggler/modal-toggler.component';
+import { CreateNoteComponent } from './components/notes/create-note/create-note.component';
+import { FilterNoteComponent } from './components/notes/filter-note/filter-note.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NotesComponent,
+    NoteComponent,
+    ModalComponent,
+    ModalTogglerComponent,
+    CreateNoteComponent,
+    FilterNoteComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
