ab9d5c62f4ddb8dcd6781c4cc737e892a00024ad
ianraphael <labicaniian@gmail.com>
2023-02-09 20:26:49 +0800

    Add FilterNoteComponent

---
+++ frontend/src/app/components/notes/filter-note/filter-note.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FilterNoteComponent } from './filter-note.component';
+
+describe('FilterNoteComponent', () => {
+  let component: FilterNoteComponent;
+  let fixture: ComponentFixture<FilterNoteComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FilterNoteComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FilterNoteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
